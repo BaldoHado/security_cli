1055088f5dff0464373a450d9b4b57a97b0c05d5
Adam Ta <adamta2004@gmail.com>
2025-02-18 22:44:16 -0800

    Add password entropy calculator

---
+++ main.py
@@ -0,0 +1,41 @@
+import math
+
+
+PRINTABLE_CHARS = {
+    "capital_letters": {chr(printable_idx) for printable_idx in range(65, 91)},
+    "lowercase_letters": {chr(printable_idx) for printable_idx in range(97, 123)},
+    "special_characters": {
+        *({chr(printable_idx) for printable_idx in range(32, 48)}),
+        *({chr(printable_idx) for printable_idx in range(58, 65)}),
+        *({chr(printable_idx) for printable_idx in range(91, 97)}),
+        *({chr(printable_idx) for printable_idx in range(123, 128)}),
+    },
+    "numbers": {chr(printable_idx) for printable_idx in range(48, 58)},
+}
+
+MAX_PRINTABLE_CHARS = 128 - 32
+
+
+def calculate_password_entropy(password: str):
+    character_set = {
+        "capital_letters": 0,
+        "lowercase_letters": 0,
+        "special_characters": 0,
+        "numbers": 0,
+    }
+    for letter in password:
+        if all(character_set.values()):
+            break
+        for name, chars in PRINTABLE_CHARS.items():
+            if letter in chars:
+                character_set[name] = len(chars)
+    return math.floor(math.log2((sum(character_set.values()) ** len(password))))
+
+
+def main():
+    entropy = calculate_password_entropy("AAAAAAAA")
+    print(entropy)
+
+
+if __name__ == "__main__":
+    main()
